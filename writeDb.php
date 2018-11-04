569ec3f8cc55102c53968a858fe386c2fb520a5d
gitsnippet <pengtao@hncfs.edu.cn>
2018-11-04 09:39:32 +0800

    add files at  Sun Nov  4 09:39:32 HKT 2018

--- writeDb.php
+++ writeDb.php
@@ -1,20 +1,22 @@
 <?php
-$goods = $_POST["goods"];
-$totalPrice = $_POST["total_price"];
-$unitPrice = $_POST["unit_price"];
-$spec = $_POST["spec"];
-$location = $_POST["location"];
-$purchaseTime = $_POST["purchaseTime"];
-$batch = $_POST["batch"];
-$comments = $_POST["comments"];
+extract($_POST);
+//$goods = $_POST["goods"];
+//$totalPrice = $_POST["totalPrice"];
+//$unitPrice = $_POST["unitPrice"];
+//$amount = $_POST["amount"];
+//$spec = $_POST["spec"];
+//$location = $_POST["location"];
+//$purchaseTime = $_POST["purchaseTime"];
+//$batch = $_POST["batch"];
+//$comments = $_POST["comments"];
 if($batch === ""){
     $batch =1;}
 
 $dsn = "mysql:host=localhost;dbname=goods";
 $con = new PDO($dsn,"root","xyzzy");
 
-$query ="insert into goods(goods,totalPrice,unitPrice,spec,location,purchaseTime,batch,comments) 
-         values('$goods','$totalPrice','$unitPrice','$spec','$location','$purchaseTime','$batch','$comments')"; 
+$query ="insert into goods(goods,totalPrice,unitPrice,amount,spec,location,purchaseTime,batch,comments) 
+         values('$goods','$totalPrice','$unitPrice','$amount','$spec','$location','$purchaseTime','$batch','$comments')"; 
 
 if($con->query($query)){
     echo "成功写入数据库";
